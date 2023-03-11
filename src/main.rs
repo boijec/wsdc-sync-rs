11393b974976b2b1e3966d3f78598f519ad96d98
Carl Boije <boijec@gmail.com>
2023-03-11 21:50:39 +0100

    feat: tokio instead of rayon
    BREAKING CHANGE: switching task model from rayon par_iters to tokio tasks
    Added debug output to map execution time of tasks
    Added error message if request to WSDC fails

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::{env};
-use std::fs::File;
-use std::io::Write;
 use std::ops::Add;
 use std::string::ToString;
-use reqwest::blocking::Client;
+use std::sync::Arc;
+use futures::future::join_all;
+use reqwest::Client;
 use serde::{Serialize, Deserialize};
 use regex::Regex;
-use rayon::prelude::*;
+use tokio::task::JoinHandle;
+use tokio::sync::Semaphore;
+use std::time::Instant;
+use tokio::fs::OpenOptions;
+use tokio::io::AsyncWriteExt;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Competitor {
@@ -14,37 +18,63 @@ struct Competitor {
     id: Option<u32>,
 }
 
-fn main() {
-    let blocking_client = Client::new();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
     let record_location = env::var("RECORD_DIRECTORY").unwrap();
     let wsdc_base = env::var("WSDC_URL").unwrap();
 
-    let mut wsdc_autocomplete = String::from(wsdc_base.clone());
+    let mut wsdc_autocomplete = String::from(&wsdc_base);
     wsdc_autocomplete = wsdc_autocomplete.add("/autocomplete?q=*");
-    let wsdc_numbers = preflight_check(wsdc_autocomplete, &blocking_client);
-
-    let mut wsdc_find = String::from(wsdc_base.clone());
-    wsdc_find = wsdc_find.add("/find?q=");
-
+    let wsdc_numbers = preflight_check(wsdc_autocomplete, &client).await;
     println!("Found {} dancers", wsdc_numbers.len());
+
+    let mut wsdc_find = String::from(&wsdc_base);
+    wsdc_find = wsdc_find.add("/find?q=");
+    let semaphore = Arc::new(Semaphore::new(21));
+    let mut tasks: Vec<JoinHandle<Result<(), ()>>>= vec![];
+    let start_time = Instant::now();
     println!("Starting download... (this may take a while)");
 
-    wsdc_numbers.par_iter()
-        .enumerate()
-        .for_each(|(_index,num)| {
-            let mut wsdc_url = String::from(wsdc_find.clone());
-            wsdc_url = wsdc_url.add(num.to_string().as_str());
-            let response = blocking_client.post(wsdc_url).send().unwrap();
-            let filename = format!("{}/{}.json", record_location, num);
-            let mut file = File::create(filename).unwrap();
-            file.write_all(response.text().unwrap().as_bytes()).unwrap();
-        });
+    for number in wsdc_numbers {
+        let client = client.clone();
+        let wsdc_find = wsdc_find.clone();
+        let record_location = record_location.clone();
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
+        let task = tokio::spawn(async move {
+            let mut wsdc_url = String::from(&wsdc_find);
+            wsdc_url = wsdc_url.add(number.to_string().as_str());
+            let response = client.post(wsdc_url).send().await.unwrap();
+            if response.status().is_success() {
+                let content = response.text().await.unwrap();
+                let filename = format!("{}/{}.json", record_location, number);
+                let mut file = OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open(filename)
+                    .await
+                    .unwrap();
 
+                file.write_all(content.as_bytes()).await.unwrap();
+            } else {
+                eprintln!("Failed to download {}", number);
+            }
+            drop(permit);
+            Ok(())
+        });
+        tasks.push(task);
+    }
+
+    join_all(tasks).await;
+
+    let elapsed_time = Instant::now().duration_since(start_time);
+    println!("Elapsed time: {:.2}s", elapsed_time.as_secs_f64());
     println!("Done!");
+    Ok(())
 }
 
-fn preflight_check(wsdc_autocomplete: String, client: &Client) -> Vec<u32> {
-    let res = client.get(wsdc_autocomplete).send().unwrap().text().unwrap();
+async fn preflight_check(wsdc_autocomplete: String, client: &Client) -> Vec<u32> {
+    let res = client.get(wsdc_autocomplete).send().await.unwrap().text().await.unwrap();
     let mut dancers: Vec<Competitor> = serde_json::from_str(res.as_str()).expect("Failed to parse JSON");
     dancers.remove(0);
     let mut wsdc_numbers: Vec<u32> = Vec::new();
